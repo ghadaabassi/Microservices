798d9f3dee30d96bebc29b6392a7f62c2c559dc7
ghada <gh.abassi01@gmail.com>
2024-05-07 10:53:00 +0100

    CustomerService

---
+++ serviceCustomer/src/test/java/com/micro/servicecustomer/ServiceCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.servicecustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
