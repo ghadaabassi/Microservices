0eb9dd45b652e9d78e7fc7c340e9b8a7ba621692
ghada <gh.abassi01@gmail.com>
2024-05-07 11:17:32 +0100

    CustomerClass

--- serviceCustomer/src/main/java/com/micro/servicecustomer/ServiceCustomerApplication.java
+++ serviceCustomer/src/main/java/com/micro/servicecustomer/ServiceCustomerApplication.java
@@ -1,8 +1,12 @@
 package com.micro.servicecustomer;
 
+import com.micro.servicecustomer.entities.Customer;
+import com.micro.servicecustomer.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
@@ -15,6 +19,19 @@ public class ServiceCustomerApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ServiceCustomerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository) {
+        return args -> {
+            repositoryRestConfiguration.exposeIdsFor(Customer.class);
+
+            //customerRepository.save(new Customer(null,"ISET","Iset@rnu.tn"));
+            //customerRepository.save(new Customer(null,"ISG","Isg@rnu.tn"));
+            //customerRepository.save(new Customer(null,"ENSI","Ensi@rnu.tn"));
+            //customerRepository.findAll().forEach(System.out::println);
+
+        };
     }
 
 
