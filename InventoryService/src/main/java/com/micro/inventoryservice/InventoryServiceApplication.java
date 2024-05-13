86f5c0b1296b55f80292e046bb842a861efe9371
ghada <gh.abassi01@gmail.com>
2024-05-13 09:54:11 +0100

    ProductApplication

--- InventoryService/src/main/java/com/micro/inventoryservice/InventoryServiceApplication.java
+++ InventoryService/src/main/java/com/micro/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,31 @@
 package com.micro.inventoryservice;
 
+import com.micro.inventoryservice.entities.Product;
+import com.micro.inventoryservice.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner start(ProductRepository prdRepository) {
+		return args -> {
+			repositoryRestConfiguration.exposeIdsFor(Product.class);
+
+			prdRepository.save(new Product(null,"Ordinateur",1500));
+			prdRepository.save(new Product(null,"Imprimante",250));
+			prdRepository.findAll().forEach(System.out::println);
+		};
 	}
 
 }
