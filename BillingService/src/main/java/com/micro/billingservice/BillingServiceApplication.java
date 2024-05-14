7b57cace8395df51c3b763ade613af3bd0e65274
ghada <gh.abassi01@gmail.com>
2024-05-14 11:07:12 +0100

    BillingService

--- BillingService/src/main/java/com/micro/billingservice/BillingServiceApplication.java
+++ BillingService/src/main/java/com/micro/billingservice/BillingServiceApplication.java
@@ -1,7 +1,15 @@
 package com.micro.billingservice;
 
+import com.micro.billingservice.entities.Bill;
+import com.micro.billingservice.entities.ProductItem;
+import com.micro.billingservice.repositories.BillRepository;
+import com.micro.billingservice.repositories.ProdcutItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class BillingServiceApplication {
@@ -9,5 +17,21 @@ public class BillingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BillingServiceApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner start(BillRepository billRepository,
+							ProdcutItemRepository prdRepository
+	) {
+		return args -> {
+			repositoryRestConfiguration.exposeIdsFor(Bill.class);
+
+			Bill bill1 = billRepository.save(new Bill(null,new Date(),1L,null));
+
+			prdRepository.save(new ProductItem(null,1L,800.0,30,bill1));
+
+			prdRepository.save(new ProductItem(null,2L,800.0,30,bill1));
+			prdRepository.save(new ProductItem(null,3L,800.0,30,bill1));
+		} ;
+	}
+}
 
 }
---
+++ BillingService/src/main/java/com/micro/billingservice/repositories/ProductItemRepository.java
@@ -0,0 +1,10 @@
+package com.micro.billingservice.repositories;
+
+import com.micro.billingservice.entities.ProductItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
+
+}
