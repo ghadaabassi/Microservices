97fdeba05ba4f137f2c92ee922343b384bd3e2bc
ghada <gh.abassi01@gmail.com>
2024-05-14 10:25:36 +0100

    GatewayServer

--- GatewayService/src/main/java/com/micro/gatewayservice/GatewayServiceApplication.java
+++ GatewayService/src/main/java/com/micro/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,11 @@ package com.micro.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -9,5 +14,14 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(
+			DiscoveryLocatorProperties dlp, ReactiveDiscoveryClient rdc) {
+		return new DiscoveryClientRouteDefinitionLocator( rdc, dlp);
+	}
+
+
 
 }
